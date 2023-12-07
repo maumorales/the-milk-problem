d4e0f57b238535c2706f86e7ee694562e0606d6f
maumorales <mau.morales@gmail.com>
2023-12-07 08:18:28 -0700

    containerized but still need to fix flyway

---
+++ .devcontainer/initdb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
+    create database milk_test;
+    create database milk_development;
+    create user milk with password 'milk';
+    grant all privileges on database milk_test to milk;
+    grant all privileges on database milk_development to milk;
+EOSQL
+#psql -f /workspace/sql/products.sql 
