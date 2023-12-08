755e119f110b1c317bfbe766c99601240371a166
maumorales <mau.morales@gmail.com>
2023-12-08 16:14:40 +0000

    add migrations to public schema in order to be consistent with the rest of the application

--- .devcontainer/initdb.sh
+++ .devcontainer/initdb.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 set -e
 psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
-    create database milk_test;
-    create database milk_development;
     create user milk with password 'milk';
+    
+    create database milk_test;
     grant all privileges on database milk_test to milk;
+    \c milk_test postgres
+    grant all on schema public to milk;
+
+    create database milk_development;
     grant all privileges on database milk_development to milk;
+    \c milk_development postgres
+    grant all on schema public to milk;
 EOSQL
